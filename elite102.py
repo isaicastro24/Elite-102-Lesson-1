09b5949a7867685e5c79051fa58ec4b945ad3d12
isaicastro24 <isai.a.castro1460@gmail.com>
2025-05-11 19:58:18 -0500

    made user creation function

--- elite102.py
+++ elite102.py
@@ -31,3 +31,29 @@ Welcome to the Steal Money Bank Account Menu!
 5. Exit
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
+
+# allows user to create a user(which is then supposed to update in database)
+
+def create_new_user():
+
+    #creating account
+    create_pin = input("Enter a PIN: ")
+    create_account_num = random.randint(1, 1000000)
+    deposit = input("Enter an amount to deposit: ")
+    new_first_name = input("Enter your first name: ")
+    new_last_name = input("Enter your last name: ")
+
+    #transfer data to sql
+    data_insertion = ("INSERT INTO userinfo" 
+                    "(pin, accountNumber, totalMoney, firstName, lastName)" 
+                    "VALUES (%s, %s, %s, %s, %s)")
+    data_insert = (create_pin, create_account_num, deposit, new_first_name, new_last_name)
+
+    #add data to sql
+    cursor.execute(data_insertion, data_insert)
+    connection.commit()
+    cursor.close()
+    
+    #friendly bank talk
+    print("Thank you for creating you account!")
+    print("Returning to main menu")
